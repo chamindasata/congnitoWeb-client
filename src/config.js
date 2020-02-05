dc4989a40db7fd1f2f742315941dfad6fd98305a
chamindasata <45987746+chamindasata@users.noreply.github.com>
2020-02-06 02:16:13 +0530

    create config file

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://cgyvhxi21i.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_X2wGOLAnA",
+    APP_CLIENT_ID: "3i15t4gbv4008imlmrhfcsndit",
+    IDENTITY_POOL_ID: "us-east-1:6d9c841e-49b1-4a18-82bd-e77f625712f5"
+  }
+};
