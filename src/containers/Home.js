541ca16b16fb950d66e29376a6ac18cff34a6ae4
chamindasata <45987746+chamindasata@users.noreply.github.com>
2020-02-06 02:08:56 +0530

    add changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,61 +1,67 @@
-import React, { Component } from 'react';
-import { PageHeader, ListGroup } from 'react-bootstrap';
-import { API } from 'aws-amplify';
-import './Home.css';
+import React, { Component } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
+import { API } from "aws-amplify";
+import "./Home.css";
 
 export default class Home extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			isLoading: true,
-			testApiCall: []
-		};
-	}
+    this.state = {
+      isLoading: true,
+      testApiCall: []
+    };
+  }
 
-	async componentDidMount() {
-		if (!this.props.isAuthenticated) {
-			return;
-		}
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
 
-		try {
-			const testApiCall = await this.testApiCall();
-			this.setState({ testApiCall });
-		} catch (e) {
-			alert(e);
-		}
+    try {
+      const testApiCall = await this.testApiCall();
+      this.setState({ testApiCall });
+    } catch (e) {
+      alert(e);
+    }
 
-		this.setState({ isLoading: false });
-	}
+    this.setState({ isLoading: false });
+  }
 
-	testApiCall() {
-		return API.get('testApiCall', '/hello');
-	}
+  testApiCall() {
+    return API.get("testApiCall", "/todos");
+  }
 
-	renderTestAPI(testApiCall) {
-		console.log(testApiCall);
-		return testApiCall.message;
-	}
+  renderTestAPI(testApiCall) {
+    console.log(testApiCall);
+    return testApiCall.message;
+  }
 
-	renderLander() {
-		return (
-			<div className="lander">
-				<h1>Test web app</h1>
-				<p>A simple react test app</p>
-			</div>
-		);
-	}
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Test web app</h1>
+        <p>A simple react test app</p>
+      </div>
+    );
+  }
 
-	renderTest() {
-		return (
-			<div className="test">
-				<PageHeader>Test API call</PageHeader>
-				<ListGroup>{!this.state.isLoading && this.renderTestAPI(this.state.testApiCall)}</ListGroup>
-			</div>
-		);
-	}
+  renderTest() {
+    return (
+      <div className="test">
+        <PageHeader>Test API call</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderTestAPI(this.state.testApiCall)}
+        </ListGroup>
+      </div>
+    );
+  }
 
-	render() {
-		return <div className="Home">{this.props.isAuthenticated ? this.renderTest() : this.renderLander()}</div>;
-	}
+  render() {
+    return (
+      <div className="Home">
+        {this.props.isAuthenticated ? this.renderTest() : this.renderLander()}
+      </div>
+    );
+  }
 }
